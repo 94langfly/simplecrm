7d2e309673fc473c826200e2591c585bd383c216
fly01 <94langfly@gmail.com>
2024-11-19 21:48:15 +0700

    default migration un comment

--- database/migrations/2024_11_18_130833_create_company_employee.php
+++ database/migrations/2024_11_18_130833_create_company_employee.php
@@ -37,6 +37,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('company_employee');
+        Schema::dropIfExists('company');
+        Schema::dropIfExists('employee');
     }
 };
